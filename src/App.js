5740c056f4178dfe6b55c8ef3f2419f7a95cdf98
Amita <panwar.amita@gmail.com>
2021-10-05 20:37:38 -0400

    added the buttons and css for the calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
+//include react-bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container,Row,Col,Button} from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input type="text" className="inputValue"></input>
+    <Container>
+    <Row>
+      <Col xs={3}><input type="button" className="Row" value="AC"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="+/-"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="%"></input></Col>
+      <Col xs={3}><input type="button" className="operators" value="/"></input></Col>
+    </Row>
+    <Row>
+      <Col xs={3}><input type="button" className="Row" value="7"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="8"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="9"></input></Col>
+      <Col xs={3}><input type="button" className="operators" value="X"></input></Col>
+    </Row>
+    <Row>
+      <Col xs={3}><input type="button" className="Row" value="4"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="5"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="6"></input></Col>
+      <Col xs={3}><input type="button" className="operators" value="-"></input></Col>
+    </Row>
+    <Row>
+      <Col xs={3}><input type="button" className="Row" value="1"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="2"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="3"></input></Col>
+      <Col xs={3}><input type="button" className="operators" value="+"></input></Col>
+    </Row>
+    <Row>
+      <Col xs={6}><input type="button" className="zero" value="0"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="."></input></Col>
+      <Col xs={3}><input type="button" className="operators" value="="></input></Col>
+    </Row>
+  </Container>
+
     </div>
   );
 }
