7f9c7ec491f1703811119f35cfa35cedda3c1e8a
Amita <panwar.amita@gmail.com>
2021-10-05 20:51:05 -0400

    added the javascript for input value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 //include react-bootstrap
+import { useEffect,useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container,Row,Col,Button} from 'react-bootstrap';
 function App() {
+  const [inputValues,setinputValues]=useState("0");
+  const setInput=(e)=>{
+setinputValues(inputValues+e.target.value);
+  }
   return (
+   
     <div className="App">
-    <input type="text" className="inputValue"></input>
+    <input type="text" className="inputValue" value={inputValues}></input>
     <Container>
     <Row>
       <Col xs={3}><input type="button" className="Row" value="AC"></input></Col>
@@ -14,21 +20,21 @@ function App() {
       <Col xs={3}><input type="button" className="operators" value="/"></input></Col>
     </Row>
     <Row>
-      <Col xs={3}><input type="button" className="Row" value="7"></input></Col>
-      <Col xs={3}><input type="button" className="Row" value="8"></input></Col>
-      <Col xs={3}><input type="button" className="Row" value="9"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="7" onClick={setInput}></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="8" onClick={setInput}></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="9" onClick={setInput}></input></Col>
       <Col xs={3}><input type="button" className="operators" value="X"></input></Col>
     </Row>
     <Row>
-      <Col xs={3}><input type="button" className="Row" value="4"></input></Col>
-      <Col xs={3}><input type="button" className="Row" value="5"></input></Col>
-      <Col xs={3}><input type="button" className="Row" value="6"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="4" onClick={setInput}></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="5" onClick={setInput}></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="6" onClick={setInput}></input></Col>
       <Col xs={3}><input type="button" className="operators" value="-"></input></Col>
     </Row>
     <Row>
-      <Col xs={3}><input type="button" className="Row" value="1"></input></Col>
-      <Col xs={3}><input type="button" className="Row" value="2"></input></Col>
-      <Col xs={3}><input type="button" className="Row" value="3"></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="1" onClick={setInput}></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="2" onClick={setInput}></input></Col>
+      <Col xs={3}><input type="button" className="Row" value="3" onClick={setInput}></input></Col>
       <Col xs={3}><input type="button" className="operators" value="+"></input></Col>
     </Row>
     <Row>
